b3ed701995bc6f05860eca8caf365828b9d6fd96
Oussama Haddi <oussama.turboxxx1@gmail.com>
2023-09-21 20:43:40 +0100

    [MOL-2] show and hide moles

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 
+let body = document.body;
+let timer = document.querySelector(".timer");
 let allMoles = document.querySelectorAll('.hole-container');
+let worm = document.getElementById("worm");
 let activeMoles = [];
+let score = 0;
+let startInterval;
 
 const spawnBebe = () => {
+    // console.log(score, worm.style.width);
+    let birdState = "hungry";
 
     let randomHole = Math.floor(Math.random() * allMoles.length);
 
@@ -13,12 +20,29 @@ const spawnBebe = () => {
     activeMoles.push(randomHole);
 
     const moleImage = document.createElement('img');
-    moleImage.src= "./assets/hungry.png"
+    moleImage.src = "./assets/hungry.png"
     moleImage.classList.add("mole");
     allMoles[randomHole].appendChild(moleImage);
-    
+
+    moleImage.addEventListener("click", () => {
+        if (birdState === "hungry") {
+            moleImage.src = "./assets/fed.png";
+            birdState = "fed";
+            score += 5;
+            worm.style.width = score + "%";
+            if (score === 50) {
+                console.log(score, worm.style.width);
+                clearInterval(startInterval);
+                alert("Game Over");
+            }
+        }
+    })
+
     setTimeout(() => {
-        moleImage.src = "./assets/sad.png";
+        if (birdState === "hungry") {
+            moleImage.classList.remove('mole_hungry');
+            moleImage.src = "./assets/sad.png";
+        }
     }, 1200);
 
     setTimeout(() => {
@@ -28,14 +52,20 @@ const spawnBebe = () => {
     setTimeout(() => {
         activeMoles = activeMoles.filter((n) =>  n != randomHole);
         moleImage.remove();
-    }, 1600);
+    }, 1700);
 
 }
 
+const startGame = () => {
+    timer.textContent = 3;
+    const countDown = setInterval(()=>{
+        --timer.textContent
+        if (parseInt(timer.textContent) === 0 ) {
+            clearInterval(countDown);
+            timer.remove();
+            startInterval = setInterval(spawnBebe, 1000);
+        }
+    }, 1000);
+}
 
-
-
-
-setInterval(spawnBebe, 1000);
-
-
+window.addEventListener("dblclick", startGame);
